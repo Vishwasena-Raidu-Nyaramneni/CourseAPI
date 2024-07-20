9b876a96b33a4d3d950de445b97201a92a7113a8
VISHWASENA RAIDU NYARAMNENI <vishwasena.nyaramneni@gmail.com>
2024-07-20 12:33:54 -0500

    Added Logger statement

--- src/main/java/com/learnspringboot/creatingapi/CreatingApiApplication.java
+++ src/main/java/com/learnspringboot/creatingapi/CreatingApiApplication.java
@@ -1,12 +1,17 @@
 package com.learnspringboot.creatingapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CreatingApiApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(CreatingApiApplication.class);
+	
 	public static void main(String[] args) {
+		logger.info("Hey Vishwa! How are you?");
 		SpringApplication.run(CreatingApiApplication.class, args);
 	}
 
