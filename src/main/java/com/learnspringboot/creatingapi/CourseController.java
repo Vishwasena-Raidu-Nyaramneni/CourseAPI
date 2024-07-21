12238bb7f284159e749c48cbe3811fb961d9718e
VISHWASENA RAIDU NYARAMNENI <vishwasena.nyaramneni@gmail.com>
2024-07-21 13:45:10 -0500

    root api

--- src/main/java/com/learnspringboot/creatingapi/CourseController.java
+++ src/main/java/com/learnspringboot/creatingapi/CourseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseController 
 {
-	@RequestMapping("/courses")
+	@RequestMapping("/")
 	public List<Course> retrieveAllCourses()
 	{	
 		return Arrays.asList(
